16929da7274a17b9f996279bea68a0c9185a1d0c
Pieter Dopheide <pjmdopheide@gmail.com>
2015-09-03 16:17:30 +0200

    Added css for the page and display images for the list contents

--- WebContent/js/controllers.js
+++ WebContent/js/controllers.js
@@ -6,7 +6,6 @@ var myAppControllers = angular.module('myAppControllers', []);
 
 myAppControllers.controller('InstrumentListCtrl', function ($scope, $http) {
 	$http.get('instruments/instruments.json').success(function(data) {
-		alert('Succes');
 		$scope.instruments = data;
 	});
 	
